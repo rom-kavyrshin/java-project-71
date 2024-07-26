e31e7abc2d6f9b82b0aae62ff893bafb4c0e0e59
Roman2 <rom.kawirshin@gmail.com>
2024-07-26 16:10:36 +0300

    Добавлены все опции и параметры для cli picocli

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,17 +1,29 @@
 package hexlet.code;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(
         name = "gendiff",
         mixinStandardHelpOptions = true,
         version = "gendiff 1.0",
-        description = "Compares two configuration files and shows a difference."
+        description = "Compares two configuration files and shows a difference.",
+        showDefaultValues = true
 )
 public class Differ implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format", defaultValue = "stylish")
+    private String format = "stylish";
 
     @Override
     public Integer call() throws Exception {
