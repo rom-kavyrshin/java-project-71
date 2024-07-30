1804868a7ab11527103423978e99667ea6d6b1a7
Roman2 <rom.kawirshin@gmail.com>
2024-07-30 17:20:46 +0300

    Добавлена зависимость jackson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
