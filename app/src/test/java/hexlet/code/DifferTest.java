013010b2221131d9a7b2767eb37ebb8f2c80c339
Roman2 <rom.kawirshin@gmail.com>
2025-04-23 18:31:01 +0300

    Вынес общий код в TestUtils

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,33 +2,20 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static hexlet.code.Differ.generate;
+import static hexlet.code.TestUtils.readFixture;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
     @Test
     public void testDiffer() throws Exception {
-        var first = readFixture("first.json");
-        var second = readFixture("second.json");
-        var expected = readFixture("expected.txt");
+        var first = readFixture("test_json", "test_addition_deletion_change", "first.json");
+        var second = readFixture("test_json", "test_addition_deletion_change", "second.json");
+        var expected = readFixture("test_json", "test_addition_deletion_change", "expected.txt");
 
         var actual = generate(first, second);
 
         assertEquals(expected, actual);
-    }
-
-    private static Path getFixturePath(String fileName) {
-        return Paths.get("src", "test", "resources", "test_json", "test_addition_deletion_change", fileName)
-                .toAbsolutePath().normalize();
-    }
-
-    private static String readFixture(String fileName) throws Exception {
-        var path = getFixturePath(fileName);
-        return Files.readString(path).trim();
     }
 }
---
+++ app/src/test/java/hexlet/code/TestUtils.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestUtils {
+
+    public static Path getFixturePath(String[] path) {
+        String[] combined = new String[path.length + 2];
+        combined[0] = "test";
+        combined[1] = "resources";
+        System.arraycopy(path, 0, combined, 2, path.length);
+        return Paths.get("src", combined)
+                .toAbsolutePath().normalize();
+    }
+
+    public static String readFixture(String... stringPath) throws Exception {
+        var path = getFixturePath(stringPath);
+        return Files.readString(path).trim();
+    }
+}
