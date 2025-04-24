692dd98c3c1c310d98b2be48602460a4b9f0a0d7
Roman2 <rom.kawirshin@gmail.com>
2025-04-24 17:53:42 +0300

    Добавлена зависимость для парсинга yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3")
 
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
