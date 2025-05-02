bb5b9505b531444d1a8de5e2ea91a4cda81db9cc
Roman2 <rom.kawirshin@gmail.com>
2025-05-02 03:22:45 +0300

    Добавлен PlainFormatter

---
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -0,0 +1,11 @@
+package hexlet.code.formatters;
+
+import hexlet.code.DiffData;
+
+public class PlainFormatter extends OutputFormatter {
+
+    @Override
+    public String format(DiffData diffData) {
+        return "";
+    }
+}
