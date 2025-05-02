5931255c01ffd109e5cdda66aca09e53b3542888
Roman2 <rom.kawirshin@gmail.com>
2025-05-02 22:00:44 +0300

    Добавлен JsonFormatter

---
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -0,0 +1,11 @@
+package hexlet.code.formatters;
+
+import hexlet.code.DiffData;
+
+public class JsonFormatter extends OutputFormatter {
+
+    @Override
+    public String format(DiffData diffData) {
+        return "";
+    }
+}
