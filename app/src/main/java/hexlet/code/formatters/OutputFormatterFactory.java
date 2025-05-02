68c4cc8721fe41c45f0b12aee2555ef5405f95d8
Roman2 <rom.kawirshin@gmail.com>
2025-05-02 18:56:34 +0300

    Добавлено создание PlainFormatter в фабрике

--- app/src/main/java/hexlet/code/formatters/OutputFormatterFactory.java
+++ app/src/main/java/hexlet/code/formatters/OutputFormatterFactory.java
@@ -5,6 +5,7 @@ import hexlet.code.exception.UnsupportedFormatException;
 public final class OutputFormatterFactory {
 
     public static final String STYLISH_OUTPUT_FORMAT_NAME = "stylish";
+    public static final String PLAIN_OUTPUT_FORMAT_NAME = "plain";
     public static final String UNSUPPORTED_FORMAT_MESSAGE = "Unsupported output format";
 
     private OutputFormatterFactory() {
@@ -13,6 +14,8 @@ public final class OutputFormatterFactory {
     public static OutputFormatter getOutputFormatter(String outputFormatName) {
         if (outputFormatName.equals(STYLISH_OUTPUT_FORMAT_NAME)) {
             return new StylishFormatter();
+        } else if (outputFormatName.equals(PLAIN_OUTPUT_FORMAT_NAME)) {
+            return new PlainFormatter();
         } else {
             throw new UnsupportedFormatException(UNSUPPORTED_FORMAT_MESSAGE + ": " + outputFormatName);
         }
